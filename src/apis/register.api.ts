b130ba204924d29dc84c68eaaeb2082495b83d50
manhdev <manhlam.mobe@gmail.com>
2023-05-10 09:23:30 +0700

    handle logic at login page, validation, mutation login api

--- src/apis/register.api.ts
+++ src/apis/register.api.ts
@@ -2,5 +2,9 @@ import { AuthResponse } from '../types/auth.type'
 import http from '../utils/http'
 
 export const registerAccount = (body: { email: string; password: string }) => {
-  return http.post<AuthResponse>('/register', body)
+  return http.post<AuthResponse>('/registerr', body)
+}
+
+export const loginAccount = (body: { email: string; password: string }) => {
+  return http.post<AuthResponse>('/login', body)
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,30 +1,70 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Schema, schema } from '../../utils/rules'
+import { useMutation } from '@tanstack/react-query'
+import { loginAccount } from '../../apis/register.api'
+import { isAxiosUnprocessableEntity } from '../../utils/utils'
+import { ResponseApi } from '../../types/utils.type'
+import Input from '../../components/Input'
+type FormState = Omit<Schema, 'confirm_password'>
 export default function Login() {
+  const loginSchema = schema.omit(['confirm_password'])
+  const {
+    formState: { errors },
+    register,
+    setError,
+    handleSubmit
+  } = useForm<FormState>({
+    resolver: yupResolver(loginSchema)
+  })
+  const loginMutation = useMutation({
+    mutationFn: (body: FormState) => loginAccount(body)
+  })
+  const onSubmit = handleSubmit((data) => {
+    //data này là data trả về khi thành công
+    loginMutation.mutate(data, {
+      onSuccess: (data) => {
+        console.log(data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntity<ResponseApi<FormState>>(error)) {
+          const formError = error.response?.data.data
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof Omit<FormState, 'confirm_password'>, {
+                type: 'Server',
+                message: formError[key as keyof Omit<FormState, 'confirm_password'>]
+              })
+            })
+          }
+        }
+      }
+    })
+  })
   return (
     <div className='bg-orange'>
       <div className='container'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='rounded bg-white p-10 shadow-sm'>
+            <form className='rounded bg-white p-10 shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng nhập</div>
-              <div className='mt-8'>
-                <input
-                  type='email'
-                  name='email'
-                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                  placeholder='Email'
-                />
-                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-              </div>
-              <div className='mt-3'>
-                <input
-                  type='password'
-                  name='password'
-                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                  placeholder='Password'
-                />
-                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-              </div>
+              <Input
+                className='mt-8'
+                name='email'
+                register={register}
+                type='email'
+                errorMessage={errors.email?.message}
+                placeholder='Email'
+              />
+              <Input
+                name='password'
+                register={register}
+                type='password'
+                errorMessage={errors.password?.message}
+                className='mt-3'
+                placeholder='Password'
+              />
               <div className='mt-3'>
                 <button className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'>
                   Đăng nhập
